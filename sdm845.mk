cac2ec2d8b41dca0fe09196b19d83d2fc376ac15
SomeHumanThatLovesFurries <xsavixander@gmail.com>
2019-09-01 23:27:51 -0400

    Added Some Camera Stuff

--- sdm845.mk
+++ sdm845.mk
@@ -138,8 +138,10 @@ PRODUCT_STATIC_BOOT_CONTROL_HAL := \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.4-service_64 \
+    libxml2 \
     Snap \
     vendor.qti.hardware.camera.device@1.0.vendor
 
